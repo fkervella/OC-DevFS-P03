567f738847336fab9f561a88d51593cec9623090
François KERVELLA <kervella.francois@gmail.com>
2025-11-24 13:00:47 +0100

    Etape 3 : Ajout de la commande list

--- main.php
+++ main.php
@@ -1,6 +1,13 @@
 <?php
+
+$allowed = ['list'];
+
 while(true) {
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
+
+    if(in_array($line, $allowed))
+        echo "Affichage de la liste\n";
+    else
+        echo "Vous avez saisi : $line\n";
 }
 ?>
