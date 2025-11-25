9db05b9e91eb0fc1a1508265879f24cedbc9ee2f
François KERVELLA <kervella.francois@gmail.com>
2025-11-25 22:35:19 +0100

    Etape 4 : Correction de la classe DBConnect et ajout de l'appel de PDO et PDO Exception

--- src/DB/DBConnect.php
+++ src/DB/DBConnect.php
@@ -4,6 +4,9 @@ namespace App\DB;
 
 require_once('config/mysql.php');
 
+use PDO;
+use PDOException;
+
 class DBConnect{
 
     private $dbPDO;
@@ -11,20 +14,21 @@ class DBConnect{
     public function __construct() {
     }
 
-    public function connexion() {
-        try {
-            c
-                sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT), 
-                MYSQL_USER, 
-                MYSQL_PASSWORD
-            );
-            $this->dbPDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch(Exception $e) {
-            echo 'Erreur : ' . $e->getMessage;
-        }
-    }
+    public function getPDO() : PDO {
 
-    public function getPDO() : \PDO {
+        if($this->dbPDO === null)
+        {
+            try {
+                $this->dbPDO = new PDO(
+                    sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT), 
+                    MYSQL_USER, 
+                    MYSQL_PASSWORD
+                );
+                $this->dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOException $e) {
+                echo 'Erreur : ' . $e->getMessage();
+            }
+        }
         return $this->dbPDO;
     }
 }
